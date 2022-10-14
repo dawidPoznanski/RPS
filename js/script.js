7abedffb3122c7595918325c0a194c27641c4e89
Dawid Poznański <dawid.poznanski@outlook.com>
2022-10-14 17:38:29 +0200

    added logic to game but dont know how to fix problems with counter, winner and other stuff

--- js/script.js
+++ js/script.js
@@ -1,12 +1,55 @@
-const btnRules = document.querySelector('.rules');
+//*Modal elements
 const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.close');
-const btnRock = document.querySelector('.rock');
-const btnPaper = document.querySelector('.paper');
-const btnScissors = document.querySelector('.scissors');
-const score = document.querySelector('.value');
+const btnRules = document.querySelector('.rules');
 
-score.textContent = 0;
+//*Game elements
+const buttons = document.querySelectorAll('.btn');
+let player;
+let computer;
+let result;
+// Score
+const score = document.querySelector('.value');
+let scoreCounter = 0;
+// Buttons to chose by player
+
+buttons.forEach(button =>
+  button.addEventListener('click', () => {
+    player = button.id;
+    computerMov();
+    score.textContent = scoreCounter;
+    console.log(checkWinner(), computerMov(), button.id);
+  })
+);
+
+function computerMov() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randomNum) {
+    case 1:
+      computer = 'rock';
+      break;
+    case 2:
+      computer = 'paper';
+      break;
+    case 3:
+      computer = 'scissors';
+      break;
+  }
+}
+
+function checkWinner() {
+  if (player == computer) {
+    return 'Draw';
+  } else if (computer == 'rock') {
+    return player == 'paper' ? 'You win' : 'You lose';
+  } else if (computer == 'paper') {
+    return player == 'scissors' ? 'You win' : 'You lose';
+  } else if (computer == 'scissors') {
+    return player == 'rock' ? 'You win' : 'You lose';
+  }
+}
+
 //Modal window
 btnRules.addEventListener('click', function () {
   modal.classList.toggle('hidden');
